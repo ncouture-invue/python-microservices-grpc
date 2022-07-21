f968ad1ab718fc25fd1a5e2744c8885ba1ce36a6
Nicolas Couture <nicolascouture@invue.com>
2022-07-21 14:42:00 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
-docker run \
-       --name marketplace \
-       -p 127.0.0.1:5000:5000/tcp \
-       marketplace
+# docker run \
+#        --name marketplace \
+#        -p 127.0.0.1:5000:5000/tcp \
+#        marketplace
 
-docker run \
-       --name recommendations \
-       --network microservices \
-       -p 127.0.0.1:50051:50051/tcp \
-       -e RECOMMENDATIONS_HOST=recommendations \
-       recommendations
+# docker run \
+#        --name recommendations \
+#        --network microservices \
+#        -p 127.0.0.1:50051:50051/tcp \
+#        -e RECOMMENDATIONS_HOST=recommendations \
+#        recommendations
+
+
+docker compose up --build
